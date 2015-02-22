8e05afd6fc48d315828db03c20f35691607b4e6e
Jennifer Lu <jenniferlu39@gmail.com>
2015-02-22 01:46:32 -0800

    history fragment created, rows should display with timestamp and main activity

---
+++ app/src/main/java/edu/ucsd/calab/extrasensory/ui/HistoryAdapter.java
@@ -0,0 +1,94 @@
+package edu.ucsd.calab.extrasensory.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import edu.ucsd.calab.extrasensory.R;
+import edu.ucsd.calab.extrasensory.data.ESContinuousActivity;
+
+/**
+ * Created by Jennifer on 2/18/2015.
+ */
+
+public class HistoryAdapter extends ArrayAdapter{
+
+    // This variable gives context as to what is calling the inflater and where it needs
+    // to inflate the view
+    private final Context context;
+    //linked list of ESContinuousActivities that need to be shown
+    private final ESContinuousActivity[] values;
+    int layoutResourceId;
+
+    /**
+     * Constructor for History Adapter
+     * @param context context from activity
+     * @param layoutResourceId The xml rowlayout
+     * @param values The ESContinuousActivity[] with the values we want to display
+     */
+    public HistoryAdapter(Context context, int layoutResourceId, ESContinuousActivity[] values) {
+        super(context, R.layout.rowlayout, values);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        ESContinuousActivityHolder holder = null;
+
+        if (row == null){
+            //Here we're creating the inflater that will be used to push in the custom view
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // This is the View that will be returned for every row --note that it inflates it with rowlayout
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            //linking labels to xml view
+            holder = new ESContinuousActivityHolder();
+            holder.time = (TextView)row.findViewById(R.id.firstLine);
+            holder.mainActivity = (TextView)row.findViewById(R.id.secondLine);
+            row.setTag(holder);
+
+            String activityLabel = "";
+            String timeLabel = "";
+            String endTimeLabel = "";
+            Date date;
+            for(ESContinuousActivity activity: values){
+                if(activity.getMainActivityUserCorrection() != null){
+                    activityLabel = activity.getMainActivityUserCorrection();
+                }
+                else{
+                    activityLabel = activity.getMainActivityServerPrediction() + "?";
+                }
+
+                //setting time label
+                date = activity.getStartTimestamp().getDateOfTimestamp();
+                timeLabel = new SimpleDateFormat("hh:mm a").format(date);
+                date = activity.getEndTimestamp().getDateOfTimestamp();
+                endTimeLabel = new SimpleDateFormat("hh:mm a").format(date);
+                timeLabel = timeLabel + " - " + endTimeLabel;
+
+               // System.out.println("adapter adding activity: " + activityLabel);
+
+                //setting activity label
+                holder.mainActivity.setText(activityLabel);
+                holder.time.setText(timeLabel);
+            }
+        }
+        return row;
+    }
+
+    static class ESContinuousActivityHolder
+    {
+        TextView time;
+        TextView mainActivity;
+    }
+}
+
--- app/src/main/java/edu/ucsd/calab/extrasensory/ui/HistoryFragment.java
+++ app/src/main/java/edu/ucsd/calab/extrasensory/ui/HistoryFragment.java
@@ -1,20 +1,26 @@
 package edu.ucsd.calab.extrasensory.ui;
 
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import edu.ucsd.calab.extrasensory.R;
+import edu.ucsd.calab.extrasensory.data.ESContinuousActivity;
+import edu.ucsd.calab.extrasensory.data.ESDatabaseAccessor;
+import edu.ucsd.calab.extrasensory.data.ESTimestamp;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
-
-
     public HistoryFragment() {
         // Required empty public constructor
     }
@@ -26,5 +32,51 @@ public class HistoryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        //getting today's activities
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY,00);
+        cal.set(Calendar.MINUTE,00);
+        cal.set(Calendar.SECOND,0);
+        cal.set(Calendar.MILLISECOND,0);
+        Date d = cal.getTime();
+
+        ESTimestamp startTime = new ESTimestamp(d);
+
+       /* for debug and check timestamp
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        System.out.println("startDATE IS " + df.format(d));
+        */
+        cal.set(Calendar.HOUR_OF_DAY,23);
+        cal.set(Calendar.MINUTE,59);
+        cal.set(Calendar.SECOND,59);
+        cal.set(Calendar.MILLISECOND,0);
+        d = cal.getTime();
+
+        ESTimestamp endTime = new ESTimestamp(d);
+
+        ESContinuousActivity [] activityList = ESDatabaseAccessor.getESDatabaseAccessor().getContinuousActivitiesFromTimeRange(startTime, endTime);
+
+        HistoryAdapter histAdapter = new HistoryAdapter(getActivity().getBaseContext(), R.layout.rowlayout, activityList);
+        // Get the list view and set it using this adapter
+        ListView listView = (ListView) getView().findViewById(R.id.listview);
+
+        LayoutInflater inflater = (LayoutInflater) getView().getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View header = inflater.inflate(R.layout.header_rowlayout, null);
+
+        //Set day title
+        TextView headerLabel = (TextView)header.findViewById(R.id.txtHeader);
+        headerLabel.setText("Today- Monday Jan 1");
+
+        listView.addHeaderView(header);
+        listView.setAdapter(histAdapter);
+    }
+
+ /*   @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // do something with the data
+    }*/
 }
