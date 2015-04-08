57040afa1dc9a3bd69fe45c8b707e07c0b6a8a88
yvaizman <yvaizman@eng.ucsd.edu>
2015-04-07 17:42:15 -0700

    settings: added seek bar for max-storage capacity

--- app/src/main/java/edu/ucsd/calab/extrasensory/ui/SettingsActivity.java
+++ app/src/main/java/edu/ucsd/calab/extrasensory/ui/SettingsActivity.java
@@ -2,21 +2,48 @@ package edu.ucsd.calab.extrasensory.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.LinkedHashMap;
 
 import edu.ucsd.calab.extrasensory.R;
 import edu.ucsd.calab.extrasensory.data.ESSettings;
 
 public class SettingsActivity extends BaseActivity {
 
-    private static float EXAMPLE_SIZE_IN_MEGABYTES = 0.24f;
+    private static final String LOG_TAG = "[SettingsActivity]";
+
+    private static final float EXAMPLE_SIZE_IN_MEGABYTES = 0.24f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        SeekBar maxStorageSeekBar = (SeekBar)findViewById(R.id.max_storage_seek_bar);
+        maxStorageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int newMaxStorageValue = Math.max(2,progress);
+                ESSettings.setMaxStoredExamples(newMaxStorageValue);
+                Log.d(LOG_TAG,"Max storage changed to " + newMaxStorageValue);
+                displayMaxStorageValue(newMaxStorageValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         setDisplayedContent();
     }
@@ -24,15 +51,21 @@ public class SettingsActivity extends BaseActivity {
     private void setDisplayedContent() {
         // Set the max storage value:
         int maxStorage = ESSettings.maxStoredExamples();
+        displayMaxStorageValue(maxStorage);
+        SeekBar maxStorageSeekBar = (SeekBar)findViewById(R.id.max_storage_seek_bar);
+        maxStorageSeekBar.setProgress(maxStorage);
+
+        // Set the UUID:
+        TextView uuid_text = (TextView)findViewById(R.id.uuid_content);
+        uuid_text.setText(ESSettings.uuid());
+    }
+
+    private void displayMaxStorageValue(int maxStorage) {
         TextView maxStorageValue = (TextView)findViewById(R.id.max_storage_value);
         float hours = ((float)maxStorage) / 60f;
         float megaBytes = ((float)maxStorage) * EXAMPLE_SIZE_IN_MEGABYTES;
         String storageString = String.format("%d (~%.2f hr, ~%.2f MB)",maxStorage,hours,megaBytes);
         maxStorageValue.setText(storageString);
-
-        // Set the UUID:
-        TextView uuid_text = (TextView)findViewById(R.id.uuid_content);
-        uuid_text.setText(ESSettings.uuid());
     }
 
     @Override
